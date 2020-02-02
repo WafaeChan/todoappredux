0a5a8905aa7ecda3881e7eca3c77b9ba48300c05
WafaeChan <58783405+WafaeChan@users.noreply.github.com>
2020-02-03 00:12:59 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Add from './components/Add';
+import List from './components/List';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Add/>
+    <List/>
+    </div>
+  );
+}
+
+export default App;
